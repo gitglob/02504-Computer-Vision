737daae1676bb4533b736fbf47c2a348e1dd6abc
Ernestas <simutisernestas@gmail.com>
2022-04-23 10:44:28 +0200

    vo begin

--- helpers.py
+++ helpers.py
@@ -119,3 +119,42 @@ def Fest_8point(q1, q2):
     U, S, V = np.linalg.svd(np.array(Bs))
     Fe = V[-1].reshape(3, 3).T * -1  # why -1?
     return Fe
+
+
+def estHomographyRANSAC(kp1, des1, kp2, des2):
+    bf = cv2.BFMatcher_create(crossCheck=True)
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key = lambda x:x.distance)
+    good = matches[:200] # just a filter, not really necessary but should speed up things
+    P1 = np.array([[kp2[m.trainIdx].pt[0],kp2[m.trainIdx].pt[1],1] for m in good])
+    P2 = np.array([[kp1[m.queryIdx].pt[0],kp1[m.queryIdx].pt[1],1] for m in good])
+    threshold = (3.84 * 3**2)**2
+    max_inliers = 0
+    Hest = None
+    best_inlier_points = []
+    inlier_indices = []
+    niter = 10
+    for i in range(niter):
+        choice  = np.random.choice(good, 4, replace=False)
+        points1 = np.array([[kp2[m.trainIdx].pt[0],kp2[m.trainIdx].pt[1],1] for m in choice])
+        points2 = np.array([[kp1[m.queryIdx].pt[0],kp1[m.queryIdx].pt[1],1] for m in choice])
+        H, status = cv2.findHomography(points1, points2)
+        inliers = 0
+        inlier_points = []
+        for i,(p1,p2) in enumerate(zip(P1,P2)):
+            p1proj = (H @ p1)
+            p1proj = p1proj / p1proj[-1]
+            p2proj = np.linalg.inv(H) @ p2
+            p2proj = p2proj / p2proj[-1]
+            d = np.linalg.norm(p2[:2] - p1proj[:2],2)**2 + np.linalg.norm(p1[:2] - p2proj[:2],2)**2
+            if d < threshold:
+                inliers += 1
+                inlier_points.append((p1,p2))
+            if inliers > max_inliers:
+                max_inliers = inliers
+                Hest = H
+                best_inlier_points = inlier_points
+                inlier_indices.append(i)
+    best_inlier_points = np.array(best_inlier_points)
+    Hest, status = cv2.findHomography(best_inlier_points[:,1,:], best_inlier_points[:,0,:])
+    return Hest
